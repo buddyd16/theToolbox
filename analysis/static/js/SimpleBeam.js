985cdaae110f5331132c7d1161e440df2793405a
buddyd16 <donnieb@gmail.com>
2022-02-27 00:44:47 -0500

    javascript for the analysis changes

--- analysis/static/js/SimpleBeam.js
+++ analysis/static/js/SimpleBeam.js
@@ -161,6 +161,106 @@ function main() {
         }
     });
 
+
+    // Add row below the first uls combindation
+    $("tbody").on("click", ".addULSComboFirst", function(){
+        
+        //get the current parent
+        var tableBody = $(this).closest("tbody");
+        
+        //get the current row
+        var $curRow = $(this).closest("tr");
+
+        //new row
+        var $newRow = '<tr class=\"ulsCombo\">' +
+                        '<td class=\"ulscombouid\">U'+ Number($("#ulscomboTable tbody tr").length + 1).toString() +'</td>' +
+                        '<td><input id=\"Dufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Dufactor\" class=\"Dufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"1\"></td>' +
+                        '<td><input id=\"Fufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Fufactor\" class=\"Fufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Lufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Lufactor\" class=\"Lufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Hufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Hufactor\" class=\"Hufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Lrufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Lrufactor\" class=\"Lrufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Sufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Sufactor\" class=\"Sufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Rufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Rufactor\" class=\"Rufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Wxufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Wxufactor\" class=\"Wxufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Wyufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Wyufactor\" class=\"Wyufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Exufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Exufactor\" class=\"Exufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Eyufactor'+Number($("#ulscomboTable tbody tr").length + 1).toString() +'\" name=\"Eyufactor\" class=\"Eyufactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td>' +
+                        '<select id="ulspattern" class="ulspattern input-sm" name="ulspattern" style="width:59px">' +
+                            '<option value="1" > y </option>' +
+                            '<option value="0" > n </option>' +
+                        '</select>' +
+                        '</td>' +
+                        '<td>' +    
+                        '<button type=\"button\" class=\"addULSCombo btn btn-secondary btn-success btn-sm\">' +
+                        '<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"14\" height=\"14\" fill=\"currentColor\" class=\"bi bi-plus\" viewBox=\"0 0 16 16\">' +
+                        '<path d=\"M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z\"/>' +
+                        '</svg>' +
+                        '</button>' +
+                        '<a href=\"#\" class=\"removeULSCombo badge badge-danger\" style=\"margin-left: 5px\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"14\" height=\"14\" fill=\"currentColor\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">' +
+                        '<path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>' +
+                        '<path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>' +
+                        '</svg></a>' +
+                        '</td>' +
+                        '</tr>';
+        
+        $curRow.after($newRow);
+
+        //expand function to renumber the remaining supports
+        tableBody.children('.ulsCombo').each(function(i) {
+        // Renumber the row
+            $(this).children("td:first").text('U'+String(i+1));
+        });
+        
+        UpdateChart();
+    });
+
+    // Add ULS combo for each button after the first one
+    $("tbody").on("click", ".addULSCombo", function(){
+        
+        //get the current parent
+        var tableBody = $(this).closest("tbody");
+        
+        //get the current row
+        var $curRow = $(this).closest("tr");
+        
+        var $newRow = $curRow.clone(true);
+        
+        $curRow.after($newRow);
+        
+        //expand function to renumber the remaining supports
+        tableBody.children('.ulsCombo').each(function(i) {
+        // Renumber the row
+          $(this).children("td:first").text('U'+String(i+1));
+        });
+        
+        UpdateChart();
+        
+    });
+
+    // Remove a ULS combo
+    $("tbody").on("click", ".removeULSCombo", function(event){
+        
+        // prevent the vertical scroll reset
+        event.preventDefault();
+
+        //get the current parent
+        var tableBody = $(this).closest("tbody");
+        
+        //remove the row
+        $(this).closest("tr").remove();
+        
+        //expand function to renumber the remaining supports
+        tableBody.children('.ulsCombo').each(function(i) {
+        // Renumber the row
+            $(this).children("td:first").text('U'+String(i+1));
+        });
+        
+        UpdateChart();
+        
+    });
+
+
     // Add row below the first service combindation
     $("tbody").on("click", ".addServiceComboFirst", function(){
         
@@ -173,17 +273,23 @@ function main() {
         //new row
         var $newRow = '<tr class=\"serviceCombo\">' +
                         '<td class=\"servicecombouid\">S'+ Number($("#servicecomboTable tbody tr").length + 1).toString() +'</td>' +
-                        '<td><input id=\"Dsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Dsfactor\" class=\"Dsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"1\"></td>' +
-                        '<td><input id=\"Fsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Fsfactor\" class=\"Fsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Lsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Lsfactor\" class=\"Lsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Hsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Hsfactor\" class=\"Hsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Lrsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Lrsfactor\" class=\"Lrsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Ssfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Ssfactor\" class=\"Ssfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Rsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Rsfactor\" class=\"Rsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Wxsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Wxsfactor\" class=\"Wxsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Wysfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Wysfactor\" class=\"Wysfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Exsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Exsfactor\" class=\"Exsfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
-                        '<td><input id=\"Eysfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Eysfactor\" class=\"Eysfactor input-sm\" style=\"width:65px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Dsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Dsfactor\" class=\"Dsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"1\"></td>' +
+                        '<td><input id=\"Fsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Fsfactor\" class=\"Fsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Lsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Lsfactor\" class=\"Lsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Hsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Hsfactor\" class=\"Hsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Lrsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Lrsfactor\" class=\"Lrsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Ssfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Ssfactor\" class=\"Ssfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Rsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Rsfactor\" class=\"Rsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Wxsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Wxsfactor\" class=\"Wxsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Wysfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Wysfactor\" class=\"Wysfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Exsfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Exsfactor\" class=\"Exsfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td><input id=\"Eysfactor'+Number($("#servicecomboTable tbody tr").length + 1).toString() +'\" name=\"Eysfactor\" class=\"Eysfactor input-sm\" style=\"width:59px\" type=\"number\" step=\"any\" value=\"0\"></td>' +
+                        '<td>' +
+                        '<select id=\"slspattern\" class=\"slspattern input-sm\" name=\"slspattern\" style=\"width:59px\">' +
+                            '<option value=\"1\"> y </option>' +
+                            '<option value=\"0\"> n </option>' +
+                        '</select>' +
+                        '</td>' +
                         '<td>' +    
                         '<button type=\"button\" class=\"addServiceCombo btn btn-secondary btn-success btn-sm\">' +
                         '<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"14\" height=\"14\" fill=\"currentColor\" class=\"bi bi-plus\" viewBox=\"0 0 16 16\">' +
